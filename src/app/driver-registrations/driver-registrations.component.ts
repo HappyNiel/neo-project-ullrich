fc7c746c7d71644e2bbfa221ab824e1f81117c27
Niel Hekkens <nielhekkens@gmail.com>
2018-05-13 22:38:32 +0200

    Create the other main components

---
+++ src/app/driver-registrations/driver-registrations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-driver-registrations',
+  templateUrl: './driver-registrations.component.html',
+  styleUrls: ['./driver-registrations.component.scss']
+})
+export class DriverRegistrationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
