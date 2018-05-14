94eb2057124774bd2522c69d232dd5afcb2653ca
Niel Hekkens <nielhekkens@gmail.com>
2018-05-14 20:12:15 +0200

    Create main components (#2)
    * Create header component and include the styles
    
    * Create the other main components
    
    * Add Travis settings so it can pass the test
    
    * Attempt 2 of fixing travis 2
    
    * Adjustments to angular.json in attempt to fix travis ci

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TeamsComponent } from './teams/teams.component';
+import { EntriesComponent } from './entries/entries.component';
+import { AdminComponent } from './admin/admin.component';
+import { UsersComponent } from './users/users.component';
+import { DriverRegistrationsComponent } from './driver-registrations/driver-registrations.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TeamsComponent,
+    EntriesComponent,
+    AdminComponent,
+    UsersComponent,
+    DriverRegistrationsComponent
   ],
   imports: [
     BrowserModule
