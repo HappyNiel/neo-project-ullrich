d7ca045a23448d30b0a49d039d550ad8d334dee9
Mike Young <mikeyoung85@gmail.com>
2018-07-14 20:00:34 -0400

    It helps to actually include the new service...

---
+++ src/app/services/team.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
+
+@Injectable()
+
+export class TeamService {
+
+  constructor(private http:HttpClient) {}
+
+  getTeams() {
+    return this.http.get('/api/team');
+  }
+}
