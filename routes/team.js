5a2e1eaac34565a10f8404951a18b6d90fd8336c
Mike Young <mikeyoung85@gmail.com>
2018-07-31 22:26:15 -0400

    better error handling

--- routes/team.js
+++ routes/team.js
@@ -2,34 +2,40 @@ var express = require('express');
 var router = express.Router();
 var TeamTable = require('../database/team');
 
-router.get('/:id', function(req, res) {
+router.get('/:id', function(req, res, next) {
     let teamId = req.params.id;
     TeamTable.findById(teamId, function(err, record){
         if(err){
             //send back error response
-            throw err;
+            next(err);
+        } else{
+            res.status(200).send(record);
         }
-        res.status(200).send(record);
+        
     })
 });
 
-router.post('/', function(req, res){
+router.post('/', function(req, res, next){
     TeamTable.createTeam(req.body, function(err, record){
         if(err){
             //send back error response
-            throw err;
+            next(err);
+        }else{
+            res.status(200).send(record);
         }
-        res.status(200).send(record);
+        
     })
 });
 
-router.get('/', function(req, res) {
+router.get('/', function(req, res, next) {
     TeamTable.getAllTeams(function(err, record){
         if(err){
             //send back error response
-            throw err;
+            next(err);
+        } else{
+            res.status(200).send(record);
         }
-        res.status(200).send(record);
+        
     })
 });
 
